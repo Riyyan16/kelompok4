b8853e08f49ceeececd25101fd17614fb6fdd66b
shelmaalifia <92342974+shelmaalifia@users.noreply.github.com>
2022-01-23 21:41:05 +0700

    Update detail.php

--- detail.php
+++ detail.php
@@ -1,14 +1,16 @@
 <?php
-   require 'database.php';
-   $bring = query("SELECT * from user");
-   $data=0;
-   $data = take($bring);
-   if($data==0)
-     echo "<script>location.href='index.php' </script>";
-  
+require 'database.php';
+$bring = query("SELECT * from user");
+$data = 0;
+$data = take($bring);
+if ($data == 0)
+    echo "<script>location.href='index.php' </script>";
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -17,14 +19,16 @@
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
+
     <div class="w3-teal" style="padding-top: 10px;">
         <div class="w3-container">
-          <div class="topnav">
-              <a href="homepage.php">Home</a>
-              <a href="profil.php">Profil</a>
-              <a href="halamanbeli.php">Beli</a>
-          </div>
+            <div class="topnav">
+                <a href="homepage.php">Home</a>
+                <a href="profil.php">Profil</a>
+                <a href="halamanbeli.php">Beli</a>
+            </div>
         </div>
     </div>
 
@@ -34,44 +38,44 @@
 
         <div class="card">
             <div class="card-content">
-            <h1 id="judul" class="namaCrpto">TKO/BIDR</h1>
-            <h5 id="pair">Lord Crypto</h5>
-            <h1 class="card-header">Beli</h1>
-            <p id="harga" >Beli di Harga : </p>
-            <p id="jumlah" >Jumlah Unit : </p>
-            <p id="total" >Total Beli : </p>
+                <div class="item" style="padding-top: 40px;">
+                    <img id="icon" src="https://indodax.com/v2/logo/png/color/btc.png" style="width: 200px; height: auto;">
+                </div>
+                <h1 id="judul" class="namaCrpto">TKO/BIDR</h1>
+                <h5 id="pair">Lord Crypto</h5>
+                <h1 class="card-header"> Detail Pembelian</h1>
+                <p id="harga">Beli di Harga : </p>
+                <p id="jumlah">Jumlah Unit : </p>
+                <p id="total">Total Beli : </p>
             </div>
         </div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="script.js"></script>
- <script>
-    var myData = localStorage['dataDetail'];
-    var myAngka = localStorage['dataAngka'];
-    var d = new Date(); // for now
-    d.getHours(); // => 9
-    d.getMinutes(); // =>  30
-    d.getSeconds(); // => 51
-    var cancel = localStorage['cancel'];
+    <script src="https://www.w3schools.com/lib/w3.js"></script>
+    <script>
+        var myData = localStorage['dataDetail'];
+        var myAngka = localStorage['dataAngka'];
+        var d = new Date(); // for now
+        d.getHours(); // => 9
+        d.getMinutes(); // =>  30
+        d.getSeconds(); // => 51
 
-    $('#date').html(`<b>Date Invoice &nbsp: </b> ${d}`)
-    $.ajax({
-        url : "https://indodax.com/api/summaries",
-        success:function(data){
-            $('#judul').html(`${data.tickers[myData].name}`)
-            $('#pair').html(`${myData} `)
-            $('#harga').html(`Beli di Harga &nbsp: ${data.tickers[myData].buy} `)
-            $('#jumlah').html(`Jumlah Unit &nbsp&nbsp: ${myAngka} `)
-            $('#total').html(`Total Beli &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ${myAngka*data.tickers[myData].buy} `)
-        },
-        error:function(err){
-            alert(err)
-        }
-    })
-    
-    
-    
- </script>
- 
+        $('#date').html(`<b>Date Invoice &nbsp: </b> ${d}`)
+        $.ajax({
+            url: "https://indodax.com/api/summaries",
+            success: function(data) {
+                $('#judul').html(`${data.tickers[myData].name}`)
+                $('#pair').html(`${myData} `)
+                $('#harga').html(`Beli di Harga &nbsp: ${data.tickers[myData].buy} `)
+                $('#jumlah').html(`Jumlah Unit &nbsp&nbsp: ${myAngka} `)
+                $('#total').html(`Total Beli &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp: ${myAngka*data.tickers[myData].buy} `)
+            },
+            error: function(err) {
+                alert(err)
+            }
+        })
+    </script>
+    <script src="script.js"></script>
 </body>
+
 </html>
